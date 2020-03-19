c13ff93c8fee45233105db91ab3c6e5e87609e1e
ChaosAlphard <Alphard450@outlook.com>
2020-03-19 22:53:58 +0800

    🔧修改微服务名称

--- irs-provider-payment8001/src/main/java/com/ilirus/springcloud/service/PaymentService.java
+++ irs-provider-payment8001/src/main/java/com/ilirus/springcloud/service/PaymentService.java
@@ -4,7 +4,7 @@ import com.ilirus.springcloud.entities.Payment;
 import org.apache.ibatis.annotations.Param;
 
 public interface PaymentService {
-    public Payment getPaymentByID(@Param("id") Long id);
+    public Payment getPaymentByID(Long id);
 
     public int createPayment(Payment payment);
 }
