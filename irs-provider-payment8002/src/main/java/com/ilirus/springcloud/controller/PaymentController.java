acfd3eb274034da16c33c27af1b686ae74d9ac9b
ChaosAlphard <Alphard450@outlook.com>
2020-03-19 22:55:19 +0800

    🔧完成支付服务8002

---
+++ irs-provider-payment8002/src/main/java/com/ilirus/springcloud/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.ilirus.springcloud.controller;
+
+import com.ilirus.springcloud.entities.CommonResult;
+import com.ilirus.springcloud.entities.Payment;
+import com.ilirus.springcloud.enums.Status;
+import com.ilirus.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/payment")
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int res = paymentService.createPayment(payment);
+        log.info("插入结果: "+res);
+
+        if(res > 0) {
+            return CommonResult.of(Status.SUCCESS);
+        } else {
+            return CommonResult.of(Status.FAIL);
+        }
+    }
+
+    @GetMapping("/query/{id}")
+    public CommonResult queryByID(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentByID(id);
+        return CommonResult.ofData(Status.SUCCESS, payment);
+    }
+}
---
+++ irs-provider-payment8002/src/main/java/com/ilirus/springcloud/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ilirus.springcloud.exception;
+
+import com.ilirus.springcloud.entities.CommonResult;
+import com.ilirus.springcloud.enums.Status;
+import com.ilirus.springcloud.exception.custom.NoThatEntityException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({NoThatEntityException.class})
+    @ResponseStatus(HttpStatus.OK)
+    public CommonResult notFound(NoThatEntityException ntee) {
+        log.error("在数据库中找不到对应的实体", ntee);
+        return CommonResult.of(Status.NO_DATA_FOUND);
+    }
+}
