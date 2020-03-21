692b53610c5ed53ca03226c927b0a4f3747c801f
ChaosAlphard <Alphard450@outlook.com>
2020-03-22 00:14:56 +0800

    ✨引入Ribbon

--- irs-consumer-order80/src/main/java/com/ilirus/springcloud/OrderMain80.java
+++ irs-consumer-order80/src/main/java/com/ilirus/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.ilirus.springcloud;
 
+import com.ilirus.springcloud.config.ApplicationontextConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name ="irs-provider-payment", configuration = {ApplicationontextConfig.class})
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
